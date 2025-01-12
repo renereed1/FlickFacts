9c766bffb5e4227d4e40a697438dac8cb5767f57
Ricky Reed <renereed1@gmail.com>
2025-01-11 21:14:11 -0800

    Updated the nav links to default to Reporting

--- frontend/src/layout/Layout.vue
+++ frontend/src/layout/Layout.vue
@@ -1,18 +1,37 @@
 <script setup>
 
+import {computed} from "vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+
+const isReportingActive = computed(() => {
+  return route.name === "reporting" || route.name === "home";
+});
 </script>
 
 <template>
 
   <main class="rounded-md h-screen flex flex-col">
 
-    <div class="space-x-10 text-center">
-      <router-link :to="{name: 'reporting'}" active-class="border px-4 py-2 rounded-md" class="px-4 py-2">Reporting
-      </router-link>
-      <router-link :to="{name: 'theaters'}" active-class="border px-4 py-2 rounded-md" class="px-4 py-2">Theaters
-      </router-link>
-      <router-link :to="{name: 'movies'}" active-class="border px-4 py-2 rounded-md" class="px-4 py-2">Movies
-      </router-link>
+    <div class="mt-2.5 ml-2.5">
+      <div class="">Flick Facts Franchise</div>
+
+      <div class="space-x-10 text-center">
+        <router-link
+            :class="[
+            'px-4 py-2',
+            isReportingActive ? 'border px-4 py-2 rounded-md' : ''
+          ]"
+            :to="{ name: 'reporting' }"
+        >
+          Reporting
+        </router-link>
+        <router-link :to="{name: 'theaters'}" active-class="border px-4 py-2 rounded-md" class="px-4 py-2">Theaters
+        </router-link>
+        <router-link :to="{name: 'movies'}" active-class="border px-4 py-2 rounded-md" class="px-4 py-2">Movies
+        </router-link>
+      </div>
     </div>
 
     <div class="flex-grow flex m-2.5 overflow-hidden">
